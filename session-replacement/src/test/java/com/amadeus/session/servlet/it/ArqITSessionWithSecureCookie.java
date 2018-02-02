0daf1fc8b7cf8fe3c7cdc500ef9ec696edf3c695
Nenad Bogojevic <nbogojevic@hotmail.com>
2018-02-02 10:28:50 +0100

    Enable force set cookie secure to true when not on secured protocol

---
+++ session-replacement/src/test/java/com/amadeus/session/servlet/it/ArqITSessionWithSecureCookie.java
@@ -0,0 +1,105 @@
+package com.amadeus.session.servlet.it;
+
+import static com.amadeus.session.servlet.it.Helpers.callWebapp;
+import static com.amadeus.session.servlet.it.Helpers.matchLines;
+import static com.amadeus.session.servlet.it.Helpers.setSessionCookie;
+import static com.amadeus.session.servlet.it.Helpers.url;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.webapp30.WebAppDescriptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@SuppressWarnings("javadoc")
+public class ArqITSessionWithSecureCookie extends AbstractITSession {
+
+  private static final String SECURE_COOKIE_WEBAPP = "secure-cookie";
+
+  private static final String NON_SECURE_COOKIE_WEBAPP = "non-secure-cookie";
+  
+  private static final String SECURE_COOKIE_ON_SECURED_REQUEST_WEBAPP = "secure-cookie-on-secured-request";
+
+  @Deployment(testable = false, name = SECURE_COOKIE_WEBAPP)
+  public static WebArchive startSecure() {
+    return Helpers.createWebApp("secure-cookie.war")
+        .addClasses(SampleServlet.class)
+        .setWebXML(new StringAsset(Descriptors.create(WebAppDescriptor.class).version("3.0")
+            .getOrCreateContextParam().paramName("com.amadeus.session.cookie.secure").paramValue("true").up()
+            .exportAsString()));
+  }
+
+  @Deployment(testable = false, name = NON_SECURE_COOKIE_WEBAPP)
+  public static WebArchive startNonSecured() {
+    return Helpers.createWebApp("non-secure-cookie.war")
+        .addClasses(SampleServlet.class)
+        .setWebXML(new StringAsset(Descriptors.create(WebAppDescriptor.class).version("3.0")
+            .getOrCreateContextParam().paramName("com.amadeus.session.cookie.secure").paramValue("false").up()
+            .exportAsString()));
+  }
+
+  @Deployment(testable = false, name = SECURE_COOKIE_ON_SECURED_REQUEST_WEBAPP)
+  public static WebArchive startSecureOnSecuredRequestOnly() {
+    return Helpers.createWebApp("secure-cookie-on-secured-request.war")
+        .addClasses(SampleServlet.class)
+        .setWebXML(new StringAsset(Descriptors.create(WebAppDescriptor.class).version("3.0")
+            .getOrCreateContextParam().paramName("com.amadeus.session.cookie.secure").paramValue("true").up()
+            .getOrCreateContextParam().paramName("com.amadeus.session.cookie.secure.on.secured.request").paramValue("true").up()
+            .exportAsString()));
+  }
+
+  @RunAsClient
+  @Test
+  @OperateOnDeployment(SECURE_COOKIE_WEBAPP)
+  public void testCookieSecured(@ArquillianResource URL baseURL) throws IOException {
+    URL urlTest = url(baseURL, "TestServlet", "testSessionPropagated");
+    String originalCookie = callWebapp(urlTest);
+    HttpURLConnection connection = (HttpURLConnection) urlTest.openConnection();
+    setSessionCookie(connection, originalCookie);
+    connection.connect();
+    List<String> cookies = connection.getHeaderFields().get("Set-Cookie");
+    assertThat(cookies, hasItem(containsString("secure")));
+  }
+
+  @RunAsClient
+  @Test
+  @OperateOnDeployment(NON_SECURE_COOKIE_WEBAPP)
+  public void testCookieNotSecured(@ArquillianResource URL baseURL) throws IOException {
+    URL urlTest = url(baseURL, "TestServlet", "testSessionPropagated");
+    String originalCookie = callWebapp(urlTest);
+    HttpURLConnection connection = (HttpURLConnection) urlTest.openConnection();
+    setSessionCookie(connection, originalCookie);
+    connection.connect();
+    List<String> cookies = connection.getHeaderFields().get("Set-Cookie");
+    assertThat(cookies, not(hasItem(containsString("secure"))));
+  }
+
+  @RunAsClient
+  @Test
+  @OperateOnDeployment(SECURE_COOKIE_ON_SECURED_REQUEST_WEBAPP)
+  public void testCookieSecureOnSecuredREquest(@ArquillianResource URL baseURL) throws IOException {
+    URL urlTest = url(baseURL, "TestServlet", "testSessionPropagated");
+    String originalCookie = callWebapp(urlTest);
+    HttpURLConnection connection = (HttpURLConnection) urlTest.openConnection();
+    setSessionCookie(connection, originalCookie);
+    connection.connect();
+    List<String> cookies = connection.getHeaderFields().get("Set-Cookie");
+    assertThat(cookies, not(hasItem(containsString("secure"))));
+  }
+}
