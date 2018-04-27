41589b334df51d5b1e45b848891eb5a17840abe7
Nenad Bogojevic <nbogojevic@amadeus.com>
2018-04-27 16:19:49 +0200

    version 0.4.11

--- session-replacement/src/main/java/com/amadeus/session/ErrorTracker.java
+++ session-replacement/src/main/java/com/amadeus/session/ErrorTracker.java
@@ -6,17 +6,18 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * The class is used to determine if the application is in error or not
  * 
  * If the number of error stored during the delay reach the max , the application is in error
- * 
- * @param period
- *          Time in milliseconds while items are kept
- * @param max
- *          If the number of items is more than the max value the Tracker is considered in error
- *
  */
 
 public class ErrorTracker {
+  /**
+   * Standard constructor
+   * 
+   * @param period
+   *          Time in milliseconds while items are kept
+   * @param max
+   *          If the number of items is more than the max value the Tracker is considered in error
+   */
   public ErrorTracker(int period, int max) {
-    super();
     this.period = period;
     this.max = max;
   }
@@ -34,12 +35,9 @@ public class ErrorTracker {
   final int max;
 
   /**
-   * The parameter is a time with the format millisecond from 1900 ( System.currentTimeMillis() ) This method add an
-   * event into the Tracker and remove all the old event with the followinf criteria
-   * 
-   * now - oldevent > period will be
-   * 
-   * removed
+   * The parameter is a time with the format millisecond from 1900 (<code>System.currentTimeMillis()</code>). This 
+   * method adds an event into the Tracker and remove all the old events with the following criteria
+   * <code>now - oldevent &gt; period</code> will be removed
    * 
    * @param now
    */
